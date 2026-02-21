30d923b6df2dd0a2f0cb8e672e574ced193e6f9c
Sonik Bariya <sonik.bariya67@gmail.com>
2026-02-21 20:59:11 +0530

    added some testing

---
+++ src/tests/tryfn-test.ts
@@ -0,0 +1,23 @@
+import * as utils from '~~/utils';
+
+async function randomPromise() {
+  return await new Promise<string>((res, rej) => {
+    if (Math.random() > 0.5) {
+      rej('Random > 0.5');
+      return;
+    }
+
+    return res('OK');
+  });
+}
+
+utils
+  .tryFn(randomPromise, { disableLogOnError: true })
+  .then(([error, data]) => {
+    if (error) {
+      console.log(error);
+      return;
+    }
+
+    console.log(data);
+  });
