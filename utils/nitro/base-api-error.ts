d564cad08e7c8af89ea5a496038a376089cc0384
Sonik Bariya <sonik.bariya67@gmail.com>
2026-02-22 23:23:44 +0530

    added determineError

--- utils/nitro/base-api-error.ts
+++ utils/nitro/base-api-error.ts
@@ -1,7 +1,9 @@
-import { createError } from 'h3';
+import { createError, H3Error } from 'h3';
+import { FetchError } from 'ofetch';
 import { capitalizeWords } from '..';
-import { HTTPStatusCodes, type HTTPStatusCode } from './status-codes';
+import { HTTPStatusCodes, type HTTPStatusCode } from '../status-codes';
 
+/** Create an throwable API friendly error (H3Error) */
 export function baseAPIError<T extends HTTPStatusCode>(
   statusCode: T,
   details: {
@@ -21,4 +23,32 @@ export function baseAPIError<T extends HTTPStatusCode>(
     statusMessage,
     data: code ? { code } : undefined,
   });
+}
+
+/** Create an Error message, can be used in catch block */
+export function determineError(error: unknown) {
+  if (error instanceof FetchError) {
+    return {
+      message: (error.data.message ?? 'Fetch error') as string,
+      payload: error,
+    };
+  }
+
+  if (error instanceof H3Error) {
+    return {
+      message: error.message ?? 'Fetch error',
+      payload: error,
+    };
+  }
+
+  if (error instanceof Error) {
+    return {
+      message: error.message,
+      payload: error,
+    };
+  }
+
+  return {
+    message: 'Something went wrong',
+  };
 }
